8426448ef1d48c4ab15627ac573456b69b7dc073
lw <wzw33@outlook.com>
2018-12-10 22:23:04 +0800

    zeroJ

---
+++ jmx/springboot-actuator-demo/src/main/java/lw/learn/jmx/springbootactuatordemo/SpringbootActuatorDemoApplication.java
@@ -0,0 +1,12 @@
+package lw.learn.jmx.springbootactuatordemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootActuatorDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootActuatorDemoApplication.class, args);
+    }
+}
---
+++ zeroJ/zeroJ-chapter2/src/test/resources/sql/customer_init.sql
@@ -0,0 +1,4 @@
+TRUNCATE customer;	
+INSERT INTO customer(`name`, contact, telephone, email, remark) VALUES("customer1", "Jack", 10086, "jack@gmail.com", NULL);
+INSERT INTO customer(`name`, contact, telephone, email, remark) VALUES("customer2", "Rose", 10010, "rose@gmail.com", NULL);
+		
