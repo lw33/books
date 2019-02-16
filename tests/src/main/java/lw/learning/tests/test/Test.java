f9913d1d44789331fac6e676bed3eb457b5c151c
lw <wzw33@outlook.com>
2019-02-16 11:17:32 +0800

    spring boot init

--- tests/src/main/java/lw/learning/tests/test/Test.java
+++ tests/src/main/java/lw/learning/tests/test/Test.java
@@ -1,5 +1,7 @@
 package lw.learning.tests.test;
 
+import java.nio.ByteBuffer;
+
 /**
  * @Author lw
  * @Date 2018-12-19 16:51:42
@@ -9,5 +11,17 @@ public class Test {
     @org.junit.Test
     public void test1() {
         System.out.println((char)29);
+    }
+
+
+    @org.junit.Test
+    public void test() {
+        ByteBuffer bytebuffer = ByteBuffer.allocate(256);
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 1000; i++) {
+            sb.append(i);
+        }
+        bytebuffer.put(sb.toString().getBytes());
+        System.out.println(bytebuffer);
     }
 }
